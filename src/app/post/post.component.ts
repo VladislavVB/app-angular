066fed6b444e33df8d1edc86b704dd0bf6976ab0
Vladislav <burdukovskij.vlad@mail.ru>
2021-12-13 17:41:32 +0300

    it's f*cking nice framework

---
+++ src/app/post/post.component.ts
@@ -0,0 +1,9 @@
+import { style } from "@angular/animations";
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.scss']
+})
+export class PostComponent {}
